7433e00867c89857a0c2afc1c12436326d899382
Edwin Melendez <boricuababyhuey@gmail.com>
2018-05-13 01:26:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var number
+
+//function add
+function add(a,b){
+  number = a + b
+return number
+}
+add();
+
+//function subtract
+function subtract(a,b){
+  number = a - b
+return number
+}
+subtract();
+
+//function multiply
+function multiply(a,b){
+  number = a * b
+return number
+}
+multiply();
+
